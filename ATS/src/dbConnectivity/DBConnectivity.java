ac352f14ffee41a2ff64cb6bb7f983fcce611c89
AlessandroBisiani <bisi90@gmail.com>
2014-03-18 15:16:18 +0000

    Update DBConnectivity.java
    Created interface for opening connection to lamp.soi.city.ac.uk
    read() and write() methods partially defined.

--- ATS/src/dbConnectivity/DBConnectivity.java
+++ ATS/src/dbConnectivity/DBConnectivity.java
@@ -2,15 +2,35 @@ package dbConnectivity;
  
 //SERIOUSLY, IS THIS CLASS USEFUL? WHY NOT JUST DBCONNECTION?
 
+import java.sql.*;
+
+
 public abstract class DBConnectivity {
 
-	public void read(){
+	Connection connection = null;
+	
+	public void read(String stmtString){
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
 		
+		connect();
+		statement = connection.prepareStatement(stmtString);
+		resultSet statement.executeQuery();
 	}
+	
+	//is having the same variable names a problem? i.e. will reads and writes happen concurrently? shouldn't right?
 	public void write(){
-		
+		PreparedStatement statement = null;
 	}
+	
 	public void connect(){
 		
+		
+		try{
+			//PASSWORD MISSING
+			connection = DriverManager.getConnection("jdbc:mysql:https://www.lamp.soi.city.ac.uk", "abpn980", "");
+		} finally {
+			
+		}
 	}
 }
