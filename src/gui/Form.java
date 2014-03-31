03483429658fa5db9c12f0bae4e1312e0b0737bd
Ale <alessandro.bisiani.1@city.ac.uk>
2014-03-31 17:46:38 +0100

    Login functionality in progress

--- src/gui/Form.java
+++ src/gui/Form.java
@@ -4,19 +4,15 @@
  */
 package gui;
 
+import dbconnection.DBConnection;
 import javax.swing.*;
+import useradministration.User;
 
 /**Plan
- * A JPanel field that holds the contents of the page.
- * The menu in each page is held in the JPanel.
- * A method for garbage collecting the JPanel and creating the new one.
+ * Every form inherits from Form, PURELY in the interest of limiting testing time of dynamically swapped JPanels. goddamnit.
  * A Form.field for holding a reference to the user.
  * A user class that holds and returns all information on a user.
  * Logging in.
- *FIRST!!! lookup initcomponents() to figure out if it interferes with adding Jpanels.
- * 2nd check for methods used for creating and destroying jpanels
- * Decide if you'll have gui inherit from jpanel and jframe, or be separate jframes.
- * consider, difficulty of  custom pages in the case of dynamically populating Frame with JPanels
  */
 
 /**
@@ -24,14 +20,28 @@ import javax.swing.*;
  * @author alessandrobisiani
  */
 public class Form extends JFrame {
+    
+    private static DBConnection dbConnection;
+    private static User user;
 
     /**
      * Creates new form Form
      */
-    public Form() {
+    public Form(/**User user*/) {
+        this.user = user;
         initComponents();
     }
 
+    public void logout(){
+        user = null;
+        new LoginPage().setVisible(true);
+        this.dispose();
+    }
+
+    public void exit(){
+        System.exit(0);
+    }
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -41,55 +51,43 @@ public class Form extends JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jButton1 = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jButton1.setText("jButton1");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(0, 927, Short.MAX_VALUE)
+            .add(layout.createSequentialGroup()
+                .add(332, 332, 332)
+                .add(jButton1)
+                .addContainerGap(498, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(0, 433, Short.MAX_VALUE)
+            .add(layout.createSequentialGroup()
+                .add(128, 128, 128)
+                .add(jButton1)
+                .addContainerGap(276, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    
-    //@param args the command line arguments
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Form.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Form.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Form.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Form.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        logout();
+    }//GEN-LAST:event_jButton1ActionPerformed
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Form().setVisible(true);
-            }
-        });
-    }
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
     // End of variables declaration//GEN-END:variables
 }
+
