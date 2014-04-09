41e553103b84cea6fe7ee1f9ea977066f394f39b
Ale <alessandro.bisiani.1@city.ac.uk>
2014-04-09 22:26:53 +0100

    trying to upload create table

--- src/dbconnection/atsDB.sql
+++ src/dbconnection/atsDB.sql
@@ -32,27 +32,28 @@ CREATE TABLE ContactDetails(
 
 CREATE TABLE DiscountPlan(
     discountPlanID INTEGER(10) AUTO_INCREMENT,
+    customerAlias VARCHAR(20),
     discountRate INTEGER(3),
     discountType VARCHAR(10),
     # lower and upper bound specify the range of values for monthly expenditure within which the given rate applies.
     lowerBound Integer(7),
     upperBound Integer(7),
-    payLaterOption INTEGER(1),
     PRIMARY KEY (discountPlanID)
 );
+
+# Adding alias as a foreign key doesn't work if re-CREATEing the whole db, and must be done separately
+# ALTER TABLE DiscountPlan ADD FOREIGN KEY (`alias`) REFERENCES Customer(`alias`)
 
 CREATE TABLE Customer(
     `alias` VARCHAR(20),
     firstName VARCHAR(20),
     lastName VARCHAR(20),
-    title VARCHAR(10),
     # null, Regular, or Valued
     customerType VARCHAR(10),
     contactID INTEGER(10),
-    discountPlanID INTEGER(10),
     PRIMARY KEY (`alias`),
     FOREIGN KEY (contactID) REFERENCES ContactDetails(contactID),
-    FOREIGN KEY (discountPlanID) REFERENCES DiscountPlan(discountPlanID)
+    FOREIGN KEY (`alias`) REFERENCES DiscountPlan(customerAlias)
 );
 
 CREATE TABLE `User`(
@@ -65,7 +66,7 @@ CREATE TABLE `User`(
 );
 	
 CREATE TABLE Commission(
-    commissionRate INTEGER(10),
+    commissionRate DECIMAL(3,2),
     dateAdded DATE,
     # by searching where dateRetired = null one can find all occurrances of commissionRates in use
     dateRetired DATE,
@@ -77,7 +78,7 @@ CREATE TABLE Commission(
 CREATE TABLE BlankType (
     # add a CHECK constraint
     blankCode INTEGER(3),
-    blankType VARCHAR(15),
+    blankType VARCHAR(30),
     PRIMARY KEY (blankCode)
 );
 	
@@ -111,7 +112,6 @@ CREATE TABLE Sale(
     `date` DATE,
     `time` TIME,
     paymentID VARCHAR(10),
-    tax DECIMAL (10, 2),
     `alias` VARCHAR(20),    
     blankID VARCHAR(11),
     commissionRate INTEGER(10),
@@ -161,118 +161,6 @@ CREATE TABLE Sale_Report(
     FOREIGN KEY (reportID) REFERENCES Report(reportID)
 );
 
--- INSERT queries
-INSERT INTO Address(addressLine1, addressLine2, county, city, country, postCode) VALUES
-    ("St Mary's Street 24", "Flat 79", "Middlesex", "London", "GB", "E34 2FG"),
-    ("High Street", "BAS", "East Sussex", "Battle", "GB", "TN33 0AD"),
-    ("Oxford Street", "Flat 61", "Westminster", "London", "GB", "W1W 8FJ"),
-    ("Doctor Street", "Flat 1", "Greater London", "London", "GB", "E14 8FF"),
-    ("Kingsland Road 149", "Flat 142", "Middlesex", "London", "GB", "E1 9FG");
 
-INSERT INTO DiscountPlan (percentage, discountType, howAwarded, payLaterOption) VALUES
-    (10, "Fixed", "Pay at end of month", 1),
-    (15, "Flexible", "Deduct from future sales", 1),
-    (0, null, null, 1),
-    (0, null, null, 0);
-
-INSERT INTO Customer VALUES
-    (null, "Oregua", "Rakina", "Miss", "Valued", 1, 1),
-    (null, "Jack", "Minchel", "Mr", "Regular", 2, 4),
-    (null, "Michael", "Mandon", "Mr", "Regular", 3, 3);
-
-INSERT INTO `User`(username, password, firstName, surname, `role`) VALUES
-    ("addmein", MD5("Greedy"), "Glara", "McJanes", "Admin"),
-    ("Jennifer", MD5("WestChance"), "Jennifer", "Alba", "Office Manager"),
-    ("Sandy", MD5("PassMyWord"), "Sandy", "Alba", "Advisor");
-
-INSERT INTO Commission(percentage, commissionType) VALUES
-    (9, "Assessable"),
-    (5, "Assessable");
-
-INSERT INTO BlankType VALUES
-    (201, "Domestic", 1);
-
-INSERT INTO Blank VALUES
-    ("20104277209", "Valid", 201, "Jennifer"),
-    ("20158426790", "Valid", 201, "Jennifer"),
-    ("20157257225", "Valid", 201, "Jennifer");
-
-INSERT INTO Sale(price, currency, valueInUSD, `date`, `time`, paymentType, tax, customerID, username, blankID, commissionID, refunded) VALUES
-    (30000, "BGL", 20, "2008-02-01", "18:32:02", "Cash", 5000, 1, "addmein", "20104277209", 1, 0),
-    (40000, "BGL", 30, "2008-02-16", "11:32:49", "Credit Card", 5000, 2, "Jennifer", "20157257225", 1, 0),
-    (50000, "BGL", 35, "2008-01-30", "14:41:25", "Cash", 5000, 1, "Sandy", "20158426790", 1, 1);
-
-INSERT INTO Report(salesOfficePlace, periodStartDate, periodEndDate, `type`) VALUES
-    ("Giday's Office", "2008-01-28", "2008-02-27", "Domestic Sales Report");
-
--- REPORT 1
--- INDIVIDUAL DOMESTIC SALES REPORT FOR USER ID 2
-SELECT @rank:=@rank+1 AS "Number", Blank.blankID AS "Original Issued Number",
-Sale.price AS "Fare Base", Sale.currency AS "Currency", Sale.valueInUSD AS "Fare Base (in USD)",
-Sale.paymentType AS "Payment Form", Sale.tax AS "Taxes", Sale.price+Sale.tax AS "Total Amounts Paid",
-Commission.percentage AS "Commission Rate", Sale.price*Commission.percentage/100 AS "Commission Amounts",
-Commission.commissionType AS "Commission Type", "" AS "Other Details", "" AS "Notes"
-FROM Blank, Sale, Commission, Report JOIN (SELECT @rank := 0) r WHERE Blank.blankID = Sale.blankID
-AND Commission.commissionID = Sale.commissionID AND Sale.refunded = 0 AND ReportID = 1 AND Sale.username = "Addmein" AND  Sale.`date` BETWEEN Report.periodStartDate AND Report.periodEndDate;
-
--- Payment Amount by Payment Form (Part of the Individual Sales Report for user id 2)
-SELECT Sale.paymentType AS "Payment Form", SUM(Sale.price+Sale.tax) AS "Total Amount Paid for a given Payment Form"
-FROM Blank, Sale, Commission, Report WHERE Blank.blankID = Sale.blankID
-AND Commission.commissionID = Sale.commissionID AND Sale.refunded = 0 AND Sale.username = "Jennifer" AND ReportID = 1 AND Sale.`date` BETWEEN Report.periodStartDate AND Report.periodEndDate;
-
--- Total Payment Amount for sales in a given period (Part of the Individual Sales Report for user id 2)
-SELECT SUM(Sale.price+Sale.tax) AS "Total Amount Paid In a Report Period"
-FROM Blank, Sale, Commission, Report WHERE Blank.blankID = Sale.blankID
-AND Commission.commissionID = Sale.commissionID AND Sale.refunded = 0 AND Sale.username = "Addmein" AND ReportID = 1 AND Sale.`date` BETWEEN Report.periodStartDate AND Report.periodEndDate;
-
--- Total Commissions per Commission Rate (Part of the Individual Sales Report for user id 2)
-SELECT Commission.percentage AS "Commission Rate", SUM(Sale.price*Commission.percentage/100) AS "Total Commission Amounts For a Report Period"
-FROM Blank, Sale, Commission, Report WHERE Blank.blankID = Sale.blankID
-AND Commission.commissionID = Sale.commissionID AND Sale.Refunded = 0 AND ReportID = 1 AND Sale.username = "Addmein" AND Sale.`date` BETWEEN Report.periodStartDate AND Report.periodEndDate;
-
--- Net Amounts For Agent Debit (Part of the Individual Sales Report for user id 2)
-SELECT SUM(Sale.price-Sale.price*Commission.percentage/100) AS "Net Amounts For Agent Debit For a Report Period"
-FROM Blank, Sale, Commission, Report WHERE Blank.blankID = Sale.blankID
-AND Commission.commissionID = Sale.commissionID AND Sale.refunded = 0 AND Sale.username = "Addmein" AND ReportID = 1 AND Sale.`date` BETWEEN Report.periodStartDate AND Report.periodEndDate;
-
--- REPORT 2
--- INDIVIDUAL DOMESTIC REFUND REPORT FOR USER ID 2
-SELECT @rank:=@rank+1 AS "Number", Blank.blankID AS "Original Issued Number",
-Sale.price AS "Fare Base", Sale.currency AS "Currency", Sale.valueInUSD AS "Fare Base (in USD)",
-Sale.paymentType AS "Payment Form", Sale.tax AS "Taxes", Sale.price+Sale.tax AS "Total Amounts Paid",
-Commission.percentage AS "Commission Rate", Sale.price*Commission.percentage/100 AS "Commission Amounts",
-Commission.commissionType AS "Commission Type", "" AS "Other Details", "" AS "Notes"
-FROM Blank, Sale, Commission, Report JOIN (SELECT @rank := 0) r WHERE Blank.blankID = Sale.blankID
-AND Commission.commissionID = Sale.commissionID AND Sale.refunded = 1 AND ReportID = 1 AND Sale.username = "Addmein" AND  Sale.`date` BETWEEN Report.periodStartDate AND Report.periodEndDate;
-
--- Payment Amount by Payment Form (Part of the Individual Refund Report for user id 2)
-SELECT Sale.paymentType AS "Payment Form", SUM(Sale.price+Sale.tax) AS "Total Amount Paid for a given Payment Form"
-FROM Blank, Sale, Commission, Report WHERE Blank.blankID = Sale.blankID
-AND Commission.commissionID = Sale.commissionID AND Sale.refunded = 1 AND Sale.username = "Addmein" AND ReportID = 1 AND Sale.`date` BETWEEN Report.periodStartDate AND Report.periodEndDate;
-
--- Total Payment Amount (Part of the Individual Refund Report for user id 2)
-SELECT SUM(Sale.price+Sale.tax) AS "Total Amount Paid In a Report Period"
-FROM Blank, Sale, Commission, Report WHERE Blank.blankID = Sale.blankID
-AND Commission.commissionID = Sale.commissionID AND Sale.refunded = 1 AND Sale.username = "Addmein" AND ReportID = 1 AND Sale.`date` BETWEEN Report.periodStartDate AND Report.periodEndDate;
-
--- Total Commissions per Commission Rate (Part of the Individual Refund Report for user id 2)
-SELECT Commission.percentage AS "Commission Rate", SUM(Sale.price*Commission.percentage/100) AS "Total Commission Amounts For a Report Period"
-FROM Blank, Sale, Commission, Report WHERE Blank.blankID = Sale.blankID
-AND Commission.commissionID = Sale.commissionID AND Sale.Refunded = 1 AND ReportID = 1 AND Sale.username = "Addmein" AND Sale.`date` BETWEEN Report.periodStartDate AND Report.periodEndDate;
-
--- Net Amounts For Agent Debit (Part of the Individual Refund Report for user id 2)
-SELECT SUM(Sale.price-Sale.price*Commission.percentage/100) AS "Net Amounts For Agent Debit For a Report Period"
-FROM Blank, Sale, Commission, Report WHERE Blank.blankID = Sale.blankID
-AND Commission.commissionID = Sale.commissionID AND Sale.refunded = 1 AND Sale.username = "Addmein" AND ReportID = 1 AND Sale.`date` BETWEEN Report.periodStartDate AND Report.periodEndDate;
-
--- DELETE queries
-DELETE FROM Sale WHERE saleID = 2;
-DELETE FROM Blank WHERE blankID = "20157257225";
-
--- SELECT queries
-SELECT * FROM Address;
-SELECT firstName, surname, `role` FROM `User`;
-
--- UPDATE queries
-UPDATE Address SET country = "United Kingdom" WHERE country = "GB";
-UPDATE `User` SET `role` = "Admin" WHERE `username` = "Sandy";
+# Adding alias as a foreign key doesn't work if re-CREATEing the whole db, and must be done separately
+# ALTER TABLE DiscountPlan ADD FOREIGN KEY (`alias`) REFERENCES Customer(`alias`)
